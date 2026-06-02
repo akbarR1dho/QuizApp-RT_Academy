e7d66a2478fdfc3d00b0f8bde0afd0db36394c22
Akbar Ridho Arrafi <akbarridhoarrafi@gmail.com>
2026-06-02 20:08:53 +0700

    Fix: Setup Vercel Again

--- api/index.php
+++ api/index.php
@@ -1,2 +1,26 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+$tmpDirs = [
+    '/tmp/storage',
+    '/tmp/storage/app',
+    '/tmp/storage/app/public',
+    '/tmp/storage/framework',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+];
+
+foreach ($tmpDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp/storage');
+
+$app->handleRequest(Illuminate\Http\Request::capture());
